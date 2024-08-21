380a64d06bf78d263f7548ca86be7a473bba91e9
TeresaCampo <terry.campo.2002@gmail.com>
2024-08-21 13:35:21 +0200

    First commit

---
+++ src/test/java/com/elaborato/simpleOnlineStore/SimpleOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elaborato.simpleOnlineStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
