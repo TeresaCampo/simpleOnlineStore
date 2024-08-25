6cc10295edcfce74a68b0607b6d673354bd9bd18
TeresaCampo <terry.campo.2002@gmail.com>
2024-08-25 16:01:21 +0200

    home.html completed

--- src/main/java/com/elaborato/simpleOnlineStore/controllers/AdminLoginController.java
+++ src/main/java/com/elaborato/simpleOnlineStore/controllers/AdminLoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminLoginController {
-    @GetMapping("/adminLogin/")
+    @GetMapping("/adminLogin")
     public String home() {
         return "adminLogin";}
 }
