7f50adf52f66465ca4f0714f127ad44f988a0cb2
TeresaCampo <terry.campo.2002@gmail.com>
2024-08-26 19:23:04 +0200

    shop.html updated: buttons and form are not displayed if the user is not authenticated as admin

--- src/main/java/com/elaborato/simpleOnlineStore/controllers/ShopController.java
+++ src/main/java/com/elaborato/simpleOnlineStore/controllers/ShopController.java
@@ -2,6 +2,7 @@ package com.elaborato.simpleOnlineStore.controllers;
 
 import com.elaborato.simpleOnlineStore.domain.dto.ArticleDto;
 import com.elaborato.simpleOnlineStore.services.ArticleService;
+import com.elaborato.simpleOnlineStore.services.SecurityService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +12,18 @@ import java.util.List;
 @Controller
 public class ShopController {
     private ArticleService articleService;
+    SecurityService securityService;
 
-    public ShopController(ArticleService articleService) {
+
+    public ShopController(ArticleService articleService, SecurityService securityService) {
         this.articleService = articleService;
+        this.securityService = securityService;
     }
 
     @GetMapping("/shop")
     public String getArticles(Model model) {
+        model.addAttribute("authenticatedUser", securityService.getAuthenticatedUserName());
+
         List<ArticleDto> articles = articleService.findAll();
         model.addAttribute("articles", articles); // Add articles to the model
         return "shop";
